c67ea9b37e2de22fb15c36bc489fd757e589abcc
tallesh <talles.espindola@gmail.com>
2023-11-17 19:21:33 +0000

    add `database` and `env` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(req, response) {
-  response.status(200).json({ chave: "tudo bão?" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows);
+  response.status(200).json({ chave: "são acima da média" });
 }
 
 export default status;
